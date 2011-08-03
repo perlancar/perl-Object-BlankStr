825f058fade4e32eacb2dedfac415014416bb199
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-03 10:30:41 +0700

    add SEE ALSO

--- lib/Object/BlankStr.pm
+++ lib/Object/BlankStr.pm
@@ -29,6 +29,7 @@ sub new { bless(\"$_[0]", $_[0]) }
     ...
  }
 
+
 =head1 DESCRIPTION
 
 Object::BlankStr is just an empty object which stringifies to "" (empty string).
@@ -39,5 +40,10 @@ So far the only case I've found this to be useful is for die()-ing without
 printing anything. If you just use 'die;' or 'die "";' Perl will print the
 default "Died at ..." message. But if you say 'die Object::BlankStr->new;' Perl
 will die without printing anything.
+
+
+=head1 SEE ALSO
+
+L<Object::NulStr>
 
 =cut

4a8823cbc5504481ec4bd9fe2866e287717615a3
Steven Haryanto (on PC) <stevenharyanto@gmail.com>
2011-08-03 10:29:25 +0700

    add exception checking example in Synopsis

--- lib/Object/BlankStr.pm
+++ lib/Object/BlankStr.pm
@@ -15,13 +15,25 @@ sub new { bless(\"$_[0]", $_[0]) }
 
  die Object::BlankStr->new; # dies without printing anything
 
+ # checking exception the right way
+ eval { die rand() > 0.5 ? Object::BlankStr->new : "blurg" };
+ my $eval_err = $@;
+ if ($eval_err || ref($eval_err)) {
+    ...
+ }
+
+ # checking exception the WRONG way, object is stringified to "" and becomes
+ # false
+ eval { die rand() > 0.5 ? Object::BlankStr->new : "blurg" };
+ if ($@) {
+    ...
+ }
 
 =head1 DESCRIPTION
 
 Object::BlankStr is just an empty object which stringifies to "" (empty string).
-Since it is an object, it has a boolean true value *in some cases*. By *in some
-cases* it means that when object stringification occurs before comparison, the
-truth value becomes false because Perl regards empty string as false.
+Since it is an object, it has a boolean true value (but be careful with
+stringification, see SYNOPSIS).
 
 So far the only case I've found this to be useful is for die()-ing without
 printing anything. If you just use 'die;' or 'die "";' Perl will print the
